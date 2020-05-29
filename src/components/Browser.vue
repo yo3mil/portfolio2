c3af507d2885afbcf7ec798d4dffc8cd13834872
Emil Wojcik <info@emilwojcik.com>
2020-05-29 22:21:26 +0100

    browser start

---
+++ src/components/Browser.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="browser">
+      <div class="browser__bar">
+          <h3>Explorer 1.1</h3>
+          <div class="browser__bar-info">
+              ?
+          </div>
+      </div>
+      <div class="browser__console">
+          .browser__console-
+          <input class="browser__console-input" type="text" placeholder="http://localhost:8080/">
+      </div>
+      <div class="browser__window">
+
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
