4ed05398b411879f87de7f5c4caf0415a9ebb037
Emil Wojcik <info@emilwojcik.com>
2020-05-31 18:11:27 +0100

    dev and home

--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="dev grid_2x3">
       <div class="dev__projects">
-        <h1>Invoicer</h1>
-        <h1>City Break</h1>
+        <h1 @click="changeTo(projectOne)" :class="{active: current === projectOne}">{{ projectOne.name }}</h1>
+        <h1 @click="changeTo(projectTwo)" :class="{active: current === projectTwo}">{{ projectTwo.name }}</h1>
         <h1>Mini Game</h1>
-        <h1>Old portfolio</h1>
-        <h1>Manda Yuen</h1>
-        <h1>Other</h1>
+        <h1 @click="changeTo(projectThree)" :class="{active: current === projectThree}">{{ projectThree.name }}</h1>
+        <h1 @click="changeTo(projectFour)" :class="{active: current === projectFour}">{{ projectFour.name }}</h1>
+        <h1 @click="changeTo(projectFive)" :class="{active: current === projectFive}">{{ projectFive.name }}</h1>
       </div>
       <div class="dev__description">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet lobortis diam, sit amet semper orci. Duis tincidunt ex in erat ullamcorper, at elementum turpis consequat. Nunc lacinia aliquam mauris sit amet tempor. Etiam vehicula arcu non felis vulputate semper. Maecenas elementum tortor fermentum ligula convallis ullamcorper. Morbi varius ullamcorper rutrum. Duis viverra commodo consequat. Suspendisse sodales suscipit posuere. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas in dignissim nunc. 
+         {{ current.description }} 
         </p>
       </div>
-      <browser></browser>
+      <browser :video="current"></browser>
   </div>
 </template>
 
@@ -23,10 +23,65 @@ import Browser from "@/components/Browser.vue";
 export default {
   components: {
     Browser
+  },
+  data() {
+    return {
+      current: {},
+      projectOne: {
+        name: "Invoicer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet lobortis diam, sit amet semper orci. Duis tincidunt ex in erat ullamcorper, at elementum turpis consequat. Nunc lacinia aliquam mauris sit amet tempor. Etiam vehicula arcu non felis vulputate semper. Maecenas elementum tortor fermentum ligula convallis",
+        video: "http://www.emilwojcik.com/video/invoicing.mp4",
+        live: "www.youtube.com",
+        git: "www.github.com"
+      },
+      projectTwo: {
+        name: "City Break",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet lobortis diam, sit amet semper orci. Duis tincidunt ex in erat ullamcorper, at elementum turpis consequat. Nunc lacinia aliquam mauris sit amet tempor. Etiam vehicula arcu non felis vulputate semper. Maecenas elementum tortor fermentum ligula convallis ullamcorper. Morbi varius ullamcorper rutrum.",
+        video: "http://www.emilwojcik.com/video/citybreak.mp4",
+        live: "www.emilwojcik.com/citybreak",
+        git: "https://github.com/yo3mil/CityBreakVue"
+      },
+      projectThree: {
+        name: "Old Portfolio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet lobortis diam, sit amet semper orci. Duis tincidunt ex in erat ullamcorper, at elementum turpis consequat. Nunc lacinia aliquam mauris sit amet tempor. Etiam vehicula arcu non felis vulputate semper. Maecenas elementum tortor fermentum ligula convallis ullamcorper. Morbi varius ullamcorper rutrum.",
+        video: "http://www.emilwojcik.com/video/portfolio.mp4",
+        live: "",
+        git: "https://github.com/yo3mil/portfolio"
+      },
+      projectFour: {
+        name: "Manda Yuen",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet lobortis diam, sit amet semper orci. Duis tincidunt ex in erat ullamcorper, at elementum turpis consequat. Nunc lacinia aliquam mauris sit amet tempor. Etiam vehicula arcu non felis vulputate semper. Maecenas elementum tortor fermentum ligula convallis ullamcorper. Morbi varius ullamcorper rutrum.",
+        video: "http://www.emilwojcik.com/video/manda.mp4",
+        live: "www.mandayuen.com",
+        git: ""
+      },
+      projectFive: {
+        name: "Other",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet lobortis diam, sit amet semper orci. Duis tincidunt ex in erat ullamcorper, at elementum turpis consequat. Nunc lacinia aliquam mauris sit amet tempor. Etiam vehicula arcu non felis vulputate semper. Maecenas elementum tortor fermentum ligula convallis ullamcorper. Morbi varius ullamcorper rutrum",
+        video: "http://www.emilwojcik.com/video/invoicer.mp4",
+        live: "",
+        git: "https://github.com/yo3mil/"
+      },
+      minigame: true
+      
+    }
+  },
+  mounted() {
+    setTimeout(()=>{
+      this.current = this.projectOne;
+    }, 500)
+    
+  },
+  methods: {
+    changeTo(project) {
+      this.current = project;
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
+  .active {
+    color: $color-black;
+  }
 </style>
