50f7a6a488a31cf41a4be14290e728bf743163ff
Emil Wojcik <info@emilwojcik.com>
2020-07-28 14:33:34 +0100

    vue-gtag added instead of vue-analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueGtag from "vue-gtag";
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 
 // analytics
