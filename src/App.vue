c77c3dd8e9cf1bdfa90705e0b054033af72917c8
yo3mil <info@emilwojcik.com>
2021-12-30 11:33:43 +0000

    breakpoints moved to va

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,74 @@
 <template>
   <div id="app" class="app grid_main">
-    <CookieMessage/>
+    <CookieMessage />
     <transition name="fade" mode="out-in">
-      <router-view/>
+      <router-view />
     </transition>
-    <router-link aria-label="Back to Home"  to="/" class="app__title">
+    <router-link aria-label="Back to Home" to="/" class="app__title">
       <h1>Emil</h1>
       <h1>WOjcik</h1>
       <transition name="fade">
-      <div v-if="back" class="app__title-return">
-        <i class="ion-arrow-return-left"></i>
-        <h3>take me back</h3>
-      </div>
+        <div v-if="back" class="app__title-return">
+          <i class="ion-arrow-return-left"></i>
+          <h3>take me back</h3>
+        </div>
       </transition>
     </router-link>
-     <Circles/>
+    <Circles />
   </div>
 </template>
 
-
 <script>
-  import "@/styles/ionicons.css"
-  import CookieMessage from "@/components/CookieMessage.vue";
-  import Circles from "@/components/Circles.vue";
-  export default {
-    components: {
-      CookieMessage,
-      Circles
-    },
-    data() {
-      return {
-        back: false
+import "@/styles/ionicons.css";
+import CookieMessage from "@/components/CookieMessage.vue";
+import Circles from "@/components/Circles.vue";
+export default {
+  components: {
+    CookieMessage,
+    Circles,
+  },
+  data() {
+    return {
+      back: false,
+    };
+  },
+  watch: {
+    $route: function() {
+      if (this.$route.path === "/") {
+        this.back = false;
+      } else {
+        this.back = true;
       }
     },
-    watch: {
-      $route: function() {
-        if (this.$route.path === "/") {
-          this.back = false;
-        } else  {
-          this.back = true;
-        }
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss">
-  
-  body {
-    font-size: 62.5%;
-    color: $color-black;
-    font-weight: 200;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-    -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-     -khtml-user-select: none; /* Konqueror HTML */
-       -moz-user-select: none; /* Old versions of Firefox */
-        -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none;
-    @media (max-width: 900px) {
-        overflow-y: scroll;
-    }
-    
-  
+body {
+  font-size: 62.5%;
+  color: $color-black;
+  font-weight: 200;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+  @media (max-width: 1025px) {
+    overflow-y: scroll;
   }
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  .app__title {
-    text-decoration: none;
-    color: $color-black;
-  }
-  
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app__title {
+  text-decoration: none;
+  color: $color-black;
+}
 </style>
--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -1,34 +1,66 @@
 <template>
   <div class="dev grid_2x3">
-      <div class="dev__projects">
-        <h1 @click="changeTo(projectOne)" :class="{active: current === projectOne}">{{ projectOne.name }}</h1>
-        <h1 @click="changeTo(projectTwo)" :class="{active: current === projectTwo}">{{ projectTwo.name }}</h1>
-        <h1 @click="changeTo(minigame)" :class="{active: current === minigame}">Mini Game</h1>
-        <h1 @click="changeTo(projectThree)" :class="{active: current === projectThree}">{{ projectThree.name }}</h1>
-        <h1 @click="changeTo(projectFour)" :class="{active: current === projectFour}">{{ projectFour.name }}</h1>
-        <h1 @click="changeTo(projectFive)" :class="{active: current === projectFive}">{{ projectFive.name }}</h1>
-      </div>
-      
-        <div v-if="toggle" class="dev__description">
-          <p>
-            {{ current.description }} 
-          </p>
-        </div>
+    <div class="dev__projects">
+      <h1
+        @click="changeTo(projectOne)"
+        :class="{ active: current === projectOne }"
+      >
+        {{ projectOne.name }}
+      </h1>
+      <h1
+        @click="changeTo(projectTwo)"
+        :class="{ active: current === projectTwo }"
+      >
+        {{ projectTwo.name }}
+      </h1>
+      <h1 @click="changeTo(minigame)" :class="{ active: current === minigame }">
+        Mini Game
+      </h1>
+      <h1
+        @click="changeTo(projectThree)"
+        :class="{ active: current === projectThree }"
+      >
+        {{ projectThree.name }}
+      </h1>
+      <h1
+        @click="changeTo(projectFour)"
+        :class="{ active: current === projectFour }"
+      >
+        {{ projectFour.name }}
+      </h1>
+      <h1
+        @click="changeTo(projectFive)"
+        :class="{ active: current === projectFive }"
+      >
+        {{ projectFive.name }}
+      </h1>
+    </div>
 
-        <!--PROJECT EXPLORER-->
-        <browser :video="current"></browser>
-        <div class="dev__mobile">
-          <div class="dev__mobile-game" v-if="current.video === true">Game is not avalaible on a mobile 🙅‍♀️</div>
-          <div v-if="current.video !== '' && current.video !== true" class="dev__mobile-link shadow-all">
-            <a :href="current.video">Video</a>
-          </div>
-          <div v-if="current.live !== ''" class="dev__mobile-link shadow-all">
-            <a :href="current.live">Live</a>
-          </div>
-          <div v-if="current.git !== ''" class="dev__mobile-link shadow-all">
-            <a :href="current.git">GitHub</a>
-          </div>
-        </div>
+    <div v-if="toggle" class="dev__description">
+      <p>
+        {{ current.description }}
+      </p>
+    </div>
+
+    <!--PROJECT EXPLORER-->
+    <browser :video="current"></browser>
+    <div class="dev__mobile">
+      <div class="dev__mobile-game" v-if="current.video === true">
+        Game is not avalaible on a mobile 🙅‍♀️
+      </div>
+      <div
+        v-if="current.video !== '' && current.video !== true"
+        class="dev__mobile-link shadow-all"
+      >
+        <a :href="current.video">Video</a>
+      </div>
+      <div v-if="current.live !== ''" class="dev__mobile-link shadow-all">
+        <a :href="current.live">Live</a>
+      </div>
+      <div v-if="current.git !== ''" class="dev__mobile-link shadow-all">
+        <a :href="current.git">GitHub</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,84 +69,89 @@ import Browser from "@/components/Browser.vue";
 
 export default {
   components: {
-    Browser
+    Browser,
   },
   data() {
     return {
       current: {},
       projectOne: {
         name: "Invoicer",
-        description: "Invoicing and product/customer management web app I made for Orientalmart.co.uk - UK’s top oriental food supplier. Lightweight, powerful and packed with useful features, it’s currently being used as their main wholesale invoicing tool.",
+        description:
+          "Invoicing and product/customer management web app I made for Orientalmart.co.uk - UK’s top oriental food supplier. Lightweight, powerful and packed with useful features, it’s currently being used as their main wholesale invoicing tool.",
         video: "https://www.emilwojcik.com/video/invoicerfinal.mp4",
         live: "",
-        git: "https://github.com/yo3mil/invoicer"
+        git: "https://github.com/yo3mil/invoicer",
       },
       projectTwo: {
         name: "City Break",
-        description: "Little widget which compares the temperatures in capital cities around Europe with the current location of its user. Originally made in vanilla, later refactored with Vue - both versions of the code are available under the GitHub link.",
+        description:
+          "Little widget which compares the temperatures in capital cities around Europe with the current location of its user. Originally made in vanilla, later refactored with Vue - both versions of the code are available under the GitHub link.",
         video: "https://www.emilwojcik.com/video/citybreak.mp4",
         live: "https://www.emilwojcik.com/citybreak",
-        git: "https://github.com/yo3mil/CityBreak"
+        git: "https://github.com/yo3mil/CityBreak",
       },
       projectThree: {
         name: "Old Portfolio",
-        description: "Pretty much the first website I have ever created. Although buggy and far from perfect, it is great to look back and see creative transitions I have managed to pull with next to zero JavaScript involved. ",
+        description:
+          "Pretty much the first website I have ever created. Although buggy and far from perfect, it is great to look back and see creative transitions I have managed to pull with next to zero JavaScript involved. ",
         video: "https://www.emilwojcik.com/video/portfolio.mp4",
         live: "",
-        git: ""
+        git: "",
       },
       projectFour: {
         name: "Manda Yuen",
-        description: "A portfolio website for UK based graphic designer. This very simple website was exactly what my client had expected from me. ",
+        description:
+          "A portfolio website for UK based graphic designer. This very simple website was exactly what my client had expected from me. ",
         video: "https://www.emilwojcik.com/video/manda.mp4",
         live: "https://www.mandayuen.com",
-        git: ""
+        git: "",
       },
       projectFive: {
         name: "Other",
-        description: "In the process of learning, I have coded tens of little projects and ideas, half of which will never see daylight. All safely stored somewhere in the depths of my computer, ready to be used as an alternative to google search. ",
+        description:
+          "In the process of learning, I have coded tens of little projects and ideas, half of which will never see daylight. All safely stored somewhere in the depths of my computer, ready to be used as an alternative to google search. ",
         video: "https://www.emilwojcik.com/video/others.mp4",
         live: "",
-        git: "https://github.com/yo3mil/"
+        git: "https://github.com/yo3mil/",
       },
       minigame: {
         video: true,
-        description: "Press start to kill some zombies and save our planet from apocalypse. Game made on “on a lap” as a single Vue component. High score: 89",
-        git: '',
-        live: ''
+        description:
+          "Press start to kill some zombies and save our planet from apocalypse. Game made on “on a lap” as a single Vue component. High score: 89",
+        git: "",
+        live: "",
       },
-      toggle: true
-    }
+      toggle: true,
+    };
   },
   mounted() {
     this.toggle = false;
     this.current = {};
     // media query for animation
-    let x = window.matchMedia("(max-width: 900px)");
-    if(x.matches) {
+    let x = window.matchMedia("(max-width: 1025px)");
+    if (x.matches) {
       this.current = this.projectOne;
-      this.toggle=true;
+      this.toggle = true;
     } else {
-      setTimeout(()=>{
+      setTimeout(() => {
         this.current = this.projectOne;
-        this.toggle=true;
-      }, 2000)
+        this.toggle = true;
+      }, 2000);
     }
-    
   },
   methods: {
     changeTo(project) {
       this.current = project;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .active {
-    color: $color-black;
-    text-decoration: underline;
-    transform: scale(1.3);
-    margin-right: .4rem;
-  }
+.active {
+  color: $color-black;
+  text-decoration: underline;
+  transform: scale(1.3);
+  margin-right: 0.4rem;
+}
 </style>
